a783c1ded53537863e599653ceaddc335a94a627
novdim <nov_dim@mail.ru>
2022-10-31 17:55:09 +0300

    add DefinitionQuarterPlane, RangeQuarter, CalculationLengthSegment

--- home_work1.py
+++ home_work1.py
@@ -1,5 +1,8 @@
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
+from math import sqrt
+
+
 def WeekendOrWeekday(number):
     if number == 6 or number == 7:
         print("Да")
@@ -27,13 +30,62 @@ VerificationOfApproval()
 
 # Пример:
 
+
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+
+def DefinitionQuarterPlane(input_X, input_Y):
+    if input_X > 0 and input_Y > 0:
+        print("1")
+    elif input_X < 0 and input_Y > 0:
+        print("2")
+    elif input_X < 0 and input_Y < 0:
+        print("3")
+    elif input_X > 0 and input_Y < 0:
+        print("4")
+
+print("Введите координаты точки X ≠ 0")
+input_X1 = int(input())
+print("Введите координаты точки Y ≠ 0")
+input_Y1 = int(input())
+
+DefinitionQuarterPlane(input_X1,input_Y1)
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+n = int(input('input quarter number: '))
+
+def RangeQuarter():
+    if n < 1 or n > 4:
+        print('Please, repeat the input')
+    elif n == 1:
+        print('x > 0 and y > 0')
+    elif n == 2:
+        print('x < 0 and y > 0')
+    elif n == 3:
+        print('x < 0 and y < 0')
+    elif n == 4:
+        print('x > 0 and y < 0')
+
+RangeQuarter()
 # Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
-# Пример:
 
+def CalculationLengthSegment(x_A, y_A, x_B, y_B):
+    print(f"Расстояние отрезка AB: {round(sqrt(((x_A - x_B)**2) + ((y_A - y_B)**2)), 2)}")
+    
+CalculationLengthSegment(3, 6, 2, 1)
+CalculationLengthSegment(7, -5, 1, -1)
+
+# Пример:
 # - A (3,6); B (2,1) -> 5,09
-# - A (7,-5); B (1,-1) -> 7,21
+# - A (7,-5); B (1,-1) ->1`` 7,21
+
+print('Введите координаты точки А:')
+x_A = float(input('X: '))
+y_A = float(input('Y: '))
+print("Введите координаты точки B:")
+x_B = float(input('X: '))
+y_B = float(input('Y: '))
+
+CalculationLengthSegment(x_A, y_A, x_B, y_B)
