6e4010142b7a0a4c5b14f7b0a33fe679a47c4a98
novdim <nov_dim@mail.ru>
2022-11-08 18:08:29 +0300

    add Negafibonacci

--- home_work3.py
+++ home_work3.py
@@ -48,6 +48,14 @@ def DifferenceMaxMinFractionalParts(list, number_decimal_places):
 
 #DifferenceMaxMinFractionalParts(list,2)
 
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+# Пример:
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
 def DecimalToBinary(number):
     print('Преобразовать десятичное числов двоичное можно используя встроенную функцию bin срезав 2 первых символа')
     print(bin(number)[2:])
@@ -59,16 +67,35 @@ def DecimalToBinary(number):
         number = number // 2
     print(numberb)
 
-DecimalToBinary(int(45))
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# DecimalToBinary(int(45))
 
-# Пример:
-
-# - 45 -> 101101
-# - 3 -> 11
-# - 2 -> 10
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 # Пример:
 
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи](https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8#:~:text=%D0%92%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B5%2C%20%D1%87%D0%B8%D1%81%D0%BB%D0%B0%20%D0%BD%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8%20%E2%80%94%20%D0%BE%D1%82%D1%80%D0%B8%D1%86%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%20%D0%B8%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D1%8B%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D1%87%D0%B8%D1%81%D0%B5%D0%BB%20%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8.)
+
+
+def Negafibonacci(n):
+    list_negafibonacci = []
+    for i in range(-n, n+1):
+        print(i)
+        if i >= 0:
+            id_x = range(i+1)
+            list = [0,1]
+            for x in id_x[2:]:
+                list.append(list[x-1] + list[x-2])
+                print(list)
+            list_negafibonacci.append(list[i])                 
+        else:
+            id_x = range((-(i-1))+1)
+            list = [1,0]
+            for x in id_x[2:]:
+                list.append(list[x-2] - list[x-1])
+                print(list)
+            list.reverse()
+            list_negafibonacci.append(list[i-2])         
+    print(list_negafibonacci)
+
+Negafibonacci(8)
+
